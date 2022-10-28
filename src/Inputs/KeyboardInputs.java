e2069a76b62adeb69de05d2b96916f0eb26aaab8
anowak4320 <anowak4320@stu.d214.org>
2022-10-28 15:24:44 -0500

    Commit

---
+++ src/Inputs/KeyboardInputs.java
@@ -0,0 +1,24 @@
+package Inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_A:
+
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
---
+++ src/Main/GamePanel.java
@@ -0,0 +1,21 @@
+package Main;
+
+import Inputs.KeyboardInputs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        addKeyListener(new KeyboardInputs());
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.fillRect(100, 100, 200, 50);
+    }
+}
